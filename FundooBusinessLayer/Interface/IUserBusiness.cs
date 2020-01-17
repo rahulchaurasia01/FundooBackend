03a4422d53c045a5c3ef50f33a7f1594d09ff43a
admin <rahulchaurasia08@gmail.com>
2020-01-17 18:53:45 +0530

    Logic for Business Layer done.

---
+++ FundooBusinessLayer/Interface/IUserBusiness.cs
@@ -0,0 +1,21 @@
+﻿using FundooCommonLayer.Model;
+using FundooCommonLayer.ModelDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundooBusinessLayer.Interface
+{
+    public interface IUserBusiness
+    {
+
+        ResponseModel Registration(UserDetails userDetails);
+
+        ResponseModel Login(LoginRequest login);
+
+        bool ForgetPassword(ForgetPasswordRequest forgetPassword);
+
+        bool ResetPassword(ResetPasswordRequest resetPassword);
+
+    }
+}
---
+++ FundooBusinessLayer/Service/UserBusiness.cs
@@ -0,0 +1,79 @@
+﻿using FundooBusinessLayer.Interface;
+using FundooCommonLayer.Model;
+using FundooCommonLayer.ModelDB;
+using FundooRepositoryLayer.Interface;
+using System;
+
+namespace FundooBusinessLayer.Service
+{
+    public class UserBusiness : IUserBusiness
+    {
+
+        private readonly IUserRepository _userRepository;
+
+        public UserBusiness(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public bool ForgetPassword(ForgetPasswordRequest forgetPassword)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(forgetPassword.EmailId))
+                    return false;
+                else
+                    return _userRepository.ForgetPassword(forgetPassword);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public ResponseModel Login(LoginRequest login)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(login.EmailId) || string.IsNullOrWhiteSpace(login.Password))
+                    return null;
+                else
+                    return _userRepository.Login(login);
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public ResponseModel Registration(UserDetails userDetails)
+        {
+            try
+            {
+                if (userDetails == null)
+                    return null;
+                else
+                    return _userRepository.Registration(userDetails);
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public bool ResetPassword(ResetPasswordRequest resetPassword)
+        {
+            try
+            {
+                if (resetPassword.ResetToken == null || resetPassword.Password == null)
+                    return false;
+                else
+                    return _userRepository.ResetPassword(resetPassword);
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
